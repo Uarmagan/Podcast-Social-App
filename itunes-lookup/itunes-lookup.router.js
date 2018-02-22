339f089db84a2a1b5a870620b8407dc4472ce07e
Ugur Armagan <ugurarmagan93@gmail.com>
2018-02-22 10:20:05 -0600

    made a specific route in router file that fetches data from iTunes lookup ID

--- itunes-lookup/itunes-lookup.router.js
+++ itunes-lookup/itunes-lookup.router.js
@@ -1,17 +1,37 @@
 const scrapingService = require('./itunes-scraping.service');
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
+var fs = require('fs');
 
 module.exports = router;
 
 router.route('/')
     .get(getListOfPodcasts);
 
+router.route('/:id')
+    .get( async (req,res) => {
+        let podcast = await getSpecificPodcast(req.params.id);
+        res.json(podcast);
+    });
+
 async function getListOfPodcasts(req, res) {
     try{
-        const arr = await scrapingService.scrapingPodcasts();
-        res.json(arr);
+        const podcastList = await scrapingService.scrapingPodcasts();
+        res.json(podcastList);
     }catch(error){
         res.send(error.message);
     }
+}
+
+function getSpecificPodcast(id){
+    const url = 'https://itunes.apple.com/lookup?id='+id;
+    const request = async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json.results[0];
+    }
+    
+    const podcastData = request();
+    return podcastData;
 }
